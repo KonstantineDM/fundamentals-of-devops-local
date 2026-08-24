4be62e1601c93861d070e5d1a6406359c976c7d6
Davydov_Konstantin <k.davydov.dev@gmail.com>
2026-08-24 11:31:14 +0400

    Add ch5 sample-app and workflow

---
+++ ch5/sample-app/eslint.config.mjs
@@ -0,0 +1,30 @@
+import js from "@eslint/js";
+import globals from "globals";
+import jest from "eslint-plugin-jest";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+    },
+  },
+
+  {
+    files: ["**/*.test.js"],
+    ...jest.configs["flat/recommended"],
+    rules: {
+      "jest/prefer-expect-assertions": "off",
+    },
+  },
+]);
